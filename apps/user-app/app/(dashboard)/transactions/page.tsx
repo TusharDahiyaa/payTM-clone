06936705c3bd5e54fd19a951111d21024671fe82
Tushar Dahiya <tushardahiya202@gmail.com>
2024-04-04 18:00:45 +0530

    first commit

--- apps/user-app/app/(dashboard)/transactions/page.tsx
+++ apps/user-app/app/(dashboard)/transactions/page.tsx
@@ -1,6 +1,66 @@
+import { getServerSession } from "next-auth";
+import prisma from "@repo/db/client";
+import { authOptions } from "../../lib/auth";
+import { P2PTransaction } from "../../../components/P2PTransactions";
 
-export default function() {
-    return <div>
+export default async function () {
+  async function sentTransactions() {
+    const session = await getServerSession(authOptions);
+    const txns = await prisma.p2pTransfer.findMany({
+      where: {
+        fromUserId: Number(session?.user?.id),
+      },
+      include: {
+        toUser: {
+          select: {
+            number: true,
+          },
+        },
+      },
+    });
+
+    return txns.map((t) => ({
+      time: t.timestamp,
+      amount: t.amount,
+      number: t.toUser?.number || "",
+    }));
+  }
+
+  async function receivedTransactions() {
+    const session = await getServerSession(authOptions);
+    const txns = await prisma.p2pTransfer.findMany({
+      where: {
+        toUserId: Number(session?.user?.id),
+      },
+      include: {
+        fromUser: {
+          select: {
+            number: true,
+          },
+        },
+      },
+    });
+    return txns.map((t) => ({
+      time: t.timestamp,
+      amount: t.amount,
+      number: t.fromUser?.number || "",
+    }));
+  }
+
+  const sendTransactions = await sentTransactions();
+  const getTransactions = await receivedTransactions();
+
+  return (
+    <div className="w-screen">
+      <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
         Transactions
+      </div>
+      <div className="mx-10">
+        <P2PTransaction
+          sendTransactions={sendTransactions}
+          getTransactions={getTransactions}
+        />
+      </div>
     </div>
+  );
 }
---
+++ apps/user-app/components/P2PTransactions.tsx
@@ -0,0 +1,74 @@
+import { Card } from "@repo/ui/card";
+
+export const P2PTransaction = ({
+  sendTransactions,
+  getTransactions,
+}: {
+  sendTransactions: {
+    time: Date;
+    amount: number;
+    number: string;
+  }[];
+  getTransactions: {
+    time: Date;
+    amount: number;
+    number: string;
+  }[];
+}) => {
+  if (!sendTransactions.length && !getTransactions.length) {
+    return (
+      <Card title="All Transactions">
+        <div className="text-center pb-8 pt-8">No Recent P2P transactions</div>
+      </Card>
+    );
+  }
+
+  return (
+    <div className="flex items-center justify evenly">
+      <div className="w-[50%] mx-5 h-lvh">
+        <Card title="Expenses">
+          <div className="h-[70vh] overflow-scroll">
+            {sendTransactions.map((t) => (
+              <div className="flex justify-between pt-2 text-sm">
+                <div>
+                  <div className="">Sent INR</div>
+                  <div className="text-slate-600">{t.time.toDateString()}</div>
+                </div>
+                <div className="flex flex-col justify-center">
+                  <div className="text-right">- Rs {t.amount / 100}</div>
+                  <div className="text-slate-600">Sent to {t.number}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </Card>
+      </div>
+      <div className="w-[50%] mx-5 h-[100lvh]">
+        <Card title="Deposits">
+          {!getTransactions.length ? (
+            <div className="text-center pb-8 pt-8">No Deposits yet</div>
+          ) : (
+            <div className="h-[70vh] overflow-scroll">
+              {getTransactions.map((t) => (
+                <div className="flex justify-between pt-2 text-sm">
+                  <div>
+                    <div className="">Received INR</div>
+                    <div className="text-slate-600">
+                      {t.time.toDateString()}
+                    </div>
+                  </div>
+                  <div className="flex flex-col justify-center">
+                    <div className="text-right">+ Rs {t.amount / 100}</div>
+                    <div className="text-slate-600">
+                      Received from {t.number}
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </Card>
+      </div>
+    </div>
+  );
+};
